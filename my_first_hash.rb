c19c095e051693e8a5e0e3dc1b940270240141a3
Hoon Lee <yl.hoony@gmail.com>
2017-11-07 08:53:22 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  {"whale bone corset" => 5, "porcelain vase" => 2, "oil painting" => 3}
+  {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
 end
 
