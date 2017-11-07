ef617242746c3b15220aac6218f5b3ca95f48a95
Hoon Lee <yl.hoony@gmail.com>
2017-11-07 08:54:33 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,6 +17,7 @@ def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
